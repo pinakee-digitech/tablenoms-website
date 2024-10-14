ba519287a57fe828f5536048f8db93d8e9724a08
arjun r <arjun.rdell@gmail.com>
2024-10-14 20:21:45 +0530

    try to force push

---
+++ src/Components/Payments/Payments.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import WorkOutlineRoundedIcon from '@mui/icons-material/WorkOutlineRounded';
+import ShieldOutlinedIcon from '@mui/icons-material/ShieldOutlined';
+import PaymentImg from '../../img/credit-card.svg';
+
+export default function Paymrnts() {
+    return (
+        <div className='w-100 d-flex justify-content-center align-items-center flex-row' style={{ height: "80vh" }}>
+            <div className='w-50 d-flex justify-content-center align-items-center flex-row'>
+                <img className='w-75' src={PaymentImg} alt="img" />
+            </div>
+            <div className='w-50 d-flex justify-content-center align-items-center flex-row'>
+                <div className='pe-4 w-75 d-flex justify-content-center align-items-start flex-column'>
+                    <h1 className='text-start'>Take payments on your terms</h1>
+                    <p>Tablenoms provides a safe and secure payment gateway which accepts UPI, credit & debit cards and all online payments.</p>
+                    <div className='d-flex justify-content-center align-items-center flex-row mb-3'>
+                        <TrendingUpIcon className='fs-1 me-2' style={{color:"#e1cb00"}}></TrendingUpIcon>
+                        <p className='m-0'>Instant credit amount in your wallet</p>
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center flex-row mb-3'>
+                        <WorkOutlineRoundedIcon className='fs-1 me-2' style={{color:"#e1cb00"}}></WorkOutlineRoundedIcon>
+                        <p className='m-0'>Split payment</p>
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center flex-row mb-3'>
+                        <ShieldOutlinedIcon className='fs-1 me-2' style={{color:"#e1cb00"}}></ShieldOutlinedIcon>
+                        <p className='m-0'>24/7 Support team</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
