d012831dfbe534c8573b67dfc9d2a1a99bcc13ae
arjun r <arjun.rdell@gmail.com>
2024-10-26 21:12:57 +0530

    now page is responsive

--- src/Components/CustomersFeedBack/CustomersFeedBack.js
+++ src/Components/CustomersFeedBack/CustomersFeedBack.js
@@ -4,13 +4,13 @@ import './CustomersFeedBack.css';
 
 export default function CustomersFeedBack() {
     return (
-        <div className="px-5 pb-5 d-flex justify-content-center flex-column align-items-start">
+        <div className="px-0 px-md-5 pb-5 d-flex justify-content-center flex-column align-items-start">
             <h6 className="px-5 text-start">Testimonials</h6>
             <h1 className="px-5 text-start pb-5">Customers are lovin' it</h1>
             <div className="px-5 d-flex justify-content-center flex-column flex-md-row">
                 <div className="cardForComments border border-light-subtle rounded-3 border-2 p-5 m-2">
                     <h5 className="pb-3">Freindly application for restaurants</h5>
-                    <p className="fs-3 text-body-secondary pb-4">This applictaion is user-friendly for both customers and us. It reduces
+                    <p className="fs-6 fs-md-3 text-body-secondary pb-4">This applictaion is user-friendly for both customers and us. It reduces
                         manpower, especially waiters and bill
                         desks. So, I recommend this system to other restaurants.</p>
                     <h5>Vivek S</h5>
@@ -18,7 +18,7 @@ export default function CustomersFeedBack() {
                 </div>
                 <div className="cardForComments border border-light-subtle rounded-3 border-2 p-5 m-2">
                     <h5 className="pb-3">Simple &amp; easy to handle</h5>
-                    <p className="fs-3 text-body-secondary pb-4">By using Tablenoms, managing the menu is as simple as doing 1st grade. By
+                    <p className="fs-6 fs-md-3 text-body-secondary pb-4">By using Tablenoms, managing the menu is as simple as doing 1st grade. By
                         using the restaurant application, modifying the menu is much quicker and easier.</p>
                     <h5>Rajesh</h5>
                     <h6 className="text-body-secondary">Naama Chettiar Veedu</h6>
--- src/Components/DigitizeYourBusiness/DigitizeYourBusiness.js
+++ src/Components/DigitizeYourBusiness/DigitizeYourBusiness.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import './DigitizeYourBusiness.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DigitizeYourBusinessImg from '../../img/digitalizYourBusiness.svg';
 
 export default function DigitizeYourBusiness() {
     return (
-        <div className='w-100 d-flex justify-content-center align-items-center flex-row' style={{ height: "80vh" }}>
-            <div className='w-50 d-flex justify-content-center align-items-center flex-row'>
-                <div className='ps-4 w-75 d-flex justify-content-center align-items-center flex-column'>
+        <div className='sub-keyFeatures my-5 my-md-0 w-100 d-flex justify-content-center align-items-center flex-wrap-reverse flex-column flex-md-row'>
+            <div className='card-width px-2 px-md-0 d-flex justify-content-center align-items-center flex-row'>
+                <div className='ps-0 ps-md-4 w-75 d-flex justify-content-center align-items-center flex-column'>
                     <h1>Sign up and digitize your business today</h1>
                     <p>It is a simple and user-friendly application that customers are able to browse, order and pay from their phones.</p>
                     <li>QR code provides a contactless dining experience. Give diners the freedom to order and reorder when they want, without waiting for a waiter.</li>
                     <li>Removal of products automatically from your e-catalog when the product is out of stock in the inventory system.</li>
                 </div>
             </div>
-            <div className='w-50 d-flex justify-content-center align-items-center flex-row'>
+            <div className='card-width d-flex justify-content-center align-items-center flex-row'>
                 <img className='w-75' src={DigitizeYourBusinessImg} alt="img" />
             </div>
         </div>
